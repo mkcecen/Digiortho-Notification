4a5ac13e529357a3e38a2a16e5d1d7aba9a785f8
Kaan ÇEÇEN <39400937+mkcecen@users.noreply.github.com>
2020-06-22 17:25:39 +0300

    first

---
+++ digiortho/lib/home.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timer_builder/timer_builder.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime alert;
+  bool isStart = false;
+  int totalPlackCount = 100;
+  int nowPlackCount = 1;
+  FlutterLocalNotificationsPlugin localNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  initializeNotifications() async {
+    var android = new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var ios = IOSInitializationSettings();
+    var initSettings = InitializationSettings(android, ios);
+    await localNotificationsPlugin.initialize(initSettings);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    alert = DateTime.now();
+    initializeNotifications();
+  }
+
+  Future singleNotification(
+      DateTime datetime, String message, String subtext, int hashcode,
+      {String sound}) async {
+    var androidChannel = AndroidNotificationDetails(
+      'channel-id',
+      'channel-name',
+      'channel-description',
+      importance: Importance.Max,
+      priority: Priority.Max,
+    );
+
+    var iosChannel = IOSNotificationDetails();
+    var platformChannel = NotificationDetails(androidChannel, iosChannel);
+    localNotificationsPlugin.schedule(
+        hashcode, message, subtext, datetime, platformChannel,
+        payload: hashcode.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme.title;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Digiortho'),
+        leading: new Container(),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: _startTimer,
+              child: Text(
+                'Zamanlayıcıyı başlat',
+                style: textStyle,
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  // isStart ? Icons.alarm_on : Icons.alarm_off,
+                  Icons.alarm_on,
+                  // color: isStart ? Colors.green : Colors.red,
+                  color: Colors.green,
+                  size: 48,
+                ),
+                TimerBuilder.periodic(
+                  Duration(seconds: 1),
+                  alignment: Duration.zero,
+                  builder: (context) {
+                    var now = DateTime.now();
+                    var difference = alert.difference(now);
+                    return Text(formatDuration(difference), style: textStyle);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.notifications),
+        //onPressed: _showNotification,
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.help),
+      //   onPressed: () {
+      //     createHelpDialog(context);
+      //   },
+      // ),
+    );
+  }
+
+  _startTimer() async {
+    isStart = true;
+    alert = DateTime.now().add(Duration(seconds: 5));
+    _showNotification(alert);
+  }
+
+  String formatDuration(Duration d) {
+    String lbl = "";
+    if (isStart == true) {
+      debugPrint("formatDuration : $d");
+      String f(int n) {
+        return n.toString().padLeft(2, '0');
+      }
+
+      d += Duration(microseconds: 999999);
+
+      lbl =
+          "Plak Değişimine Kalan\n${f(d.inDays)} gün ${f(d.inMinutes % 60)}:${f(d.inSeconds % 60)}";
+
+      if (d.inSeconds == 0) {
+        _changeValue();
+      }
+    } else {
+      lbl = "Plak Değişimine Kalan\n 00 gün 00:00";
+    }
+    return lbl;
+  }
+
+  _changeValue() {
+    isStart = false;
+    nowPlackCount += 1;
+  }
+
+  _showNotification(DateTime time) async {
+    DateTime now = DateTime.now().toUtc().add(
+          Duration(seconds: 5),
+        );
+    await singleNotification(
+      now,
+      '$nowPlackCount . Plak Değişimi',
+      'Plak değiştime zamanın geldi. Sakın unutma :) ',
+      98123871,
+    );
+  }
+
+  createHelpDialog(BuildContext context) {
+    TextEditingController customController = TextEditingController();
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Nasıl Kullanırım ?'),
+          content: Text(
+              'Zamanlayıcıyı başlat butonu ile sayaç çalışmaya başlar ve her 15 günde bir sana hatırlatma gönderir.\n\nHer hatırlatma sonrası sayacı tekrar başlatmalısın yoksa 2 saatte bir sana uyarı mesajı gelecektir :) '),
+          actions: <Widget>[
+            MaterialButton(
+              elevation: 5.0,
+              child: Text('Tamam'),
+              onPressed: () {
+                Navigator.of(context).pop(customController.text.toString());
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ digiortho/lib/main.dart
@@ -0,0 +1,98 @@
+import 'dart:async';
+
+import 'package:digiorthoo/home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      theme:
+          ThemeData(primaryColor: Colors.blueGrey, accentColor: Colors.blueGrey),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ));
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () => print("Splach Done!"));
+    Future.delayed(const Duration(seconds: 3), () async {
+      Navigator.push(
+          context, new MaterialPageRoute(builder: (context) => new HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/logo.jpg',
+                        width: 225,
+                        height: 225,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "Telsiz Ortodonti Sistemi",
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(
+                      valueColor:
+                          new AlwaysStoppedAnimation<Color>(Colors.black),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text(
+                      "Kaan Çeçen",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
