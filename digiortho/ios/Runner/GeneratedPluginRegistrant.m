7e0547ba46e4ea47c321f480199216604cbaa26d
Kaan ÇEÇEN <39400937+mkcecen@users.noreply.github.com>
2020-06-23 20:35:26 +0300

    build bug

--- digiortho/ios/Runner/GeneratedPluginRegistrant.m
+++ digiortho/ios/Runner/GeneratedPluginRegistrant.m
@@ -10,8 +10,8 @@
 @import flutter_local_notifications;
 #endif
 
-#if __has_include(<path_provider/PathProviderPlugin.h>)
-#import <path_provider/PathProviderPlugin.h>
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
 #else
 @import path_provider;
 #endif
